e1a8e4c5f792ace2a1fb1729c6104969572be2bf
Ivandaza04 <ivandaza2004@gmail.com>
2024-09-05 19:16:53 -0300

    arreglo supabes URL

--- src/app/utils/supabase.js
+++ src/app/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://vwdaxauhqygjjtpphleh.supabase.co/";
+const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3ZGF4YXVocXlnamp0cHBobGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3ODY2OTEsImV4cCI6MjAzMTM2MjY5MX0.c5sNo_KOaC0ZN69WxlbUDTV5vYUOUx_J6kZKNFS8ALM"
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
